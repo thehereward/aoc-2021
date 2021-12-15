284417533b33c065265e22ca5134063e5781fc66
Hereward Mills <herewardnjmills@gmail.com>
2021-12-15 06:19:14 +0000

    Day 15 - but a bit better

--- 15.js
+++ 15.js
@@ -2,114 +2,32 @@ function parseData(data) {
   return data.map((d) => d.split("").map((char) => parseInt(char)));
 }
 
+function initialise2DArray(width, height, contents) {
+  var newArray = new Array(height);
+  for (var i = 0; i < width; i = i + 1) {
+    newArray[i] = new Array(width).fill(contents);
+  }
+  return newArray;
+}
+
 function setRisk(riskMap, data, risk, j, i) {
+  var somethingChanged = false;
   const existingRisk = riskMap[j][i];
   const newRisk = risk + data[j][i];
   if (existingRisk == undefined) {
     riskMap[j][i] = newRisk;
+    somethingChanged = true;
   } else if (newRisk < existingRisk) {
     riskMap[j][i] = newRisk;
+    somethingChanged = true;
   }
-}
-
-function solvePart1(data) {
-  console.log("Solving Part 1");
-  data = parseData(data);
-
-  var height = data.length;
-  var width = data[0].length;
-  console.log({ width, height });
-
-  var riskMap = new Array(height);
-  for (var i = 0; i < width; i = i + 1) {
-    riskMap[i] = new Array(width).fill();
-  }
-
-  riskMap[0][0] = 0;
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  const finalRisk = riskMap[height - 1][width - 1];
-  console.log({ finalRisk });
+  return somethingChanged;
 }
 
 function expandData(data) {
   var height = data.length * 5;
   var width = data[0].length * 5;
-  var newData = new Array(height);
-  for (var i = 0; i < width; i = i + 1) {
-    newData[i] = new Array(width).fill();
-  }
+  var newData = initialise2DArray(width, height);
 
   for (var j = 0; j < data.length; j = j + 1) {
     for (var i = 0; i < data[0].length; i = i + 1) {
@@ -126,304 +44,73 @@ function expandData(data) {
   return newData;
 }
 
+function updateRisk(riskMap, data, height, width) {
+  var somethingChanged = false;
+  for (var j = 0; j < height; j = j + 1) {
+    for (var i = 0; i < width; i = i + 1) {
+      const risk = riskMap[j][i];
+      if (i > 0) {
+        somethingChanged =
+          setRisk(riskMap, data, risk, j, i - 1) || somethingChanged;
+      }
+      if (i < width - 1) {
+        somethingChanged =
+          setRisk(riskMap, data, risk, j, i + 1) || somethingChanged;
+      }
+      if (j > 0) {
+        somethingChanged =
+          setRisk(riskMap, data, risk, j - 1, i) || somethingChanged;
+      }
+      if (j < height - 1) {
+        somethingChanged =
+          setRisk(riskMap, data, risk, j + 1, i) || somethingChanged;
+      }
+    }
+  }
+  if (somethingChanged) {
+    return updateRisk(riskMap, data, height, width);
+  } else {
+    return riskMap;
+  }
+}
+
+function findSmallestPath(data) {
+  var height = data.length;
+  var width = data[0].length;
+
+  var riskMap = initialise2DArray(width, height);
+
+  riskMap[0][0] = 0;
+  riskMap = updateRisk(riskMap, data, height, width);
+
+  const finalRisk = riskMap[height - 1][width - 1];
+  return finalRisk;
+}
+
+function solvePart1(data) {
+  console.log("Solving Part 1");
+  data = parseData(data);
+
+  const finalRisk = findSmallestPath(data);
+
+  console.log({ finalRisk });
+}
+
 function solvePart2(data) {
   console.log("Solving Part 2");
   data = parseData(data);
   data = expandData(data);
-  // console.log(data[data.length - 1].join(""));
-  // process.exit();
-  var height = data.length;
-  var width = data[0].length;
-  console.log({ width, height });
 
-  var riskMap = new Array(height);
-  for (var i = 0; i < width; i = i + 1) {
-    riskMap[i] = new Array(width).fill();
-  }
+  const finalRisk = findSmallestPath(data);
 
-  riskMap[0][0] = 0;
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  for (var j = 0; j < height; j = j + 1) {
-    for (var i = 0; i < width; i = i + 1) {
-      const risk = riskMap[j][i];
-      if (i > 0) {
-        setRisk(riskMap, data, risk, j, i - 1);
-      }
-      if (i < width - 1) {
-        setRisk(riskMap, data, risk, j, i + 1);
-      }
-      if (j > 0) {
-        setRisk(riskMap, data, risk, j - 1, i);
-      }
-      if (j < height - 1) {
-        setRisk(riskMap, data, risk, j + 1, i);
-      }
-    }
-  }
-
-  const finalRisk = riskMap[height - 1][width - 1];
   console.log({ finalRisk });
 }
 
 function solve(data, partTwo) {
   if (!partTwo) {
     return solvePart1(data);
-    // 531 is incorrect
   } else {
     return solvePart2(data);
-    // 2888 is too high
   }
 }
 
