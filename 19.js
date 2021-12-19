1240438b37a10cc0215900ab5d23c9ee640eaea4
Hereward Mills <herewardnjmills@gmail.com>
2021-12-19 16:41:20 +0000

    Maybe working?

--- 19.js
+++ 19.js
@@ -16,6 +16,9 @@ function parseData(data) {
       scanners[scanners.length - 1].push(split(line));
     }
   });
+  scanners = scanners.map((d, index) => {
+    return { scanner: index, beacons: d };
+  });
   return scanners;
 }
 
@@ -172,96 +175,251 @@ function getTransformation(matchingBeacons0, matchingBeacons1) {
   return transform;
 }
 
-function solvePart1(data) {
-  console.log("Solving Part 1");
-  data = parseData(data);
-  var scanner0 = data[0];
-  var scanner1 = data[1];
-  var scanner0Diffs = getVectorsBetweenBeacons(scanner0);
-  var scanner1Diffs = getVectorsBetweenBeacons(scanner1);
+function getMapping(targetComponent, sourceVector) {
+  var absoluteValues = sourceVector.map((char) => Math.abs(char));
+  // console.log({ absoluteValues });
+  var index = absoluteValues.indexOf(Math.abs(targetComponent));
+  var isInverse = sourceVector[index] == targetComponent * -1;
 
-  var counts = scanner0Diffs.map((vectorsFromBeacon0) => {
-    var count = scanner1Diffs.map((beacon) => {
-      return countMatchingMagnitudes(vectorsFromBeacon0, beacon);
-    });
-    const maxCount = Math.max(...count);
-    if (maxCount >= 12) {
-      count = count.indexOf(maxCount);
-    } else {
-      count = undefined;
+  // console.log({ index, isInverse });
+  return function (vectorToMap) {
+    if (isInverse) {
+      return -vectorToMap[index];
     }
-    return count;
-  });
+    return vectorToMap[index];
+  };
+}
 
-  var matchingBeacons0 = [];
-  var matchingBeacons1 = [];
-  counts.forEach((value, index) => {
-    if (value != undefined) {
-      matchingBeacons0.push(scanner0[index]);
-      matchingBeacons1.push(scanner1[value]);
-    }
-  });
+function getMappingFunction(targetVector, sourceVector) {
+  var xMap = getMapping(targetVector[0], sourceVector);
+  var yMap = getMapping(targetVector[1], sourceVector);
+  var zMap = getMapping(targetVector[2], sourceVector);
 
-  console.log({ matchingBeacons0 });
-  console.log({ matchingBeacons1 });
+  return function (vectorToMap) {
+    return [xMap(vectorToMap), yMap(vectorToMap), zMap(vectorToMap)];
+  };
+}
 
-  const diff0_01 = subtract(matchingBeacons0[1], matchingBeacons0[0]);
-  const diff1_01 = subtract(matchingBeacons1[1], matchingBeacons1[0]);
-  console.log({ diff0_01, diff1_01 });
+function getMatchedBeacons(setOne, setTwo) {
+  var mapOfMatchingBeacons = getMapOfMatchingBeacons(setOne, setTwo);
+  var { matchingBeaconsBaseSet, matchingBeaconsNewSet } = getBeacons(
+    mapOfMatchingBeacons,
+    setOne,
+    setTwo
+  );
+  return {
+    matchingBeaconsBaseSet,
+    matchingBeaconsNewSet,
+    thereAreMatches: matchingBeaconsBaseSet.length > 0,
+  };
+}
 
-  function getMapping(aa, bb) {
-    var absbb = bb.map((char) => Math.abs(char));
-    var index = absbb.indexOf(Math.abs(aa));
-    var isInverse = bb[index] == aa * -1;
-    console.log({ index, isInverse });
+function fooBar(baseBeaconSet, input) {
+  console.log(`Scanner: ${input.scanner}`);
+  const newBeacons = input.beacons;
 
-    return function (cc) {
-      if (isInverse) {
-        return -cc[index];
-      }
-      return cc[index];
+  var { matchingBeaconsBaseSet, matchingBeaconsNewSet } = getMatchedBeacons(
+    baseBeaconSet,
+    newBeacons
+  );
+
+  if (matchingBeaconsBaseSet.length == 0) {
+    return {
+      mappedBeacons: [],
     };
   }
 
-  var xMap = getMapping(diff0_01[0], diff1_01);
-  var yMap = getMapping(diff0_01[1], diff1_01);
-  var zMap = getMapping(diff0_01[2], diff1_01);
+  const vectorInKnownSystem = subtract(
+    matchingBeaconsBaseSet[5],
+    matchingBeaconsBaseSet[0]
+  );
+  const vectorInUnknownSystem = subtract(
+    matchingBeaconsNewSet[5],
+    matchingBeaconsNewSet[0]
+  );
 
-  var newX = xMap(diff1_01);
-  var newY = yMap(diff1_01);
-  var newZ = zMap(diff1_01);
-  console.log({ newX, newY, newZ });
-  // const diff0_02 = subtract(matchingBeacons0[2], matchingBeacons0[0]);
-  // const diff1_02 = subtract(matchingBeacons1[2], matchingBeacons1[0]);
-  // console.log({ diff0_02, diff1_02 });
-  return;
-  // var transform = getTransformation(matchingBeacons0, matchingBeacons1);
-  var transform = rotateY(rotateY(identity(3)));
-  console.log(transform);
-  transform = matrix(transform);
+  const mappingFunction = getMappingFunction(
+    vectorInKnownSystem,
+    vectorInUnknownSystem
+  );
+  const beacons1Matched = matchingBeaconsNewSet.map((b) => mappingFunction(b));
 
-  var transformed = matchingBeacons1.map((b) => {
-    return multiply(transform, b)
-      .toArray()
-      .map((c) => Math.round(c));
-  });
+  const offset = subtract(matchingBeaconsBaseSet[0], beacons1Matched[0]);
 
-  console.log({ transform: transform.toArray() });
-  console.log({ matchingBeacons0 });
-  console.log({ transformed });
-
-  return;
-  var isMatch = isSameCoordinateSystem(matchingBeacons0, matchingBeacons1);
-
-  if (!isMatch) {
-    matchingBeacons1 = matchingBeacons1.map((beacon) => {
-      return rotateY(rotateY(beacon));
-    });
+  console.log({ offset });
+  if (input.scanner == 4) {
+    var mapMatched = beacons1Matched.map((b) => add(offset, b));
+    console.log({ mapMatched });
   }
 
-  isMatch = isSameCoordinateSystem(matchingBeacons0, matchingBeacons1);
+  // matchingBeacons0.forEach((value, index) => {
+  //   console.log(subtract(value, beacons1Matched[index]));
+  // });
 
-  console.log({ isMatch });
+  const mappedBeacons = newBeacons.map((beacon) => add(offset, beacon));
+  return {
+    mappedBeacons,
+  };
+}
+
+function mapMatchedBeaconSet(matchingBeaconsBaseSet, matchingBeaconsNewSet) {
+  const vectorInKnownSystem = subtract(
+    matchingBeaconsBaseSet[5],
+    matchingBeaconsBaseSet[0]
+  );
+  const vectorInUnknownSystem = subtract(
+    matchingBeaconsNewSet[5],
+    matchingBeaconsNewSet[0]
+  );
+
+  const rotationFunction = getMappingFunction(
+    vectorInKnownSystem,
+    vectorInUnknownSystem
+  );
+  var mappedBeacons = matchingBeaconsNewSet.map((b) => rotationFunction(b));
+
+  const offset = subtract(matchingBeaconsBaseSet[0], mappedBeacons[0]);
+  mappedBeacons = mappedBeacons.map((m) => add(m, offset));
+
+  function mappingFunction(vectorToMap) {
+    var result = rotationFunction(vectorToMap);
+    return add(result, offset);
+  }
+
+  return { mappingFunction, offset, mappedBeacons };
+}
+
+function getBeacons(mapOfMatchingBeacons, baseBeaconSet, newBeacons) {
+  var matchingBeaconsBaseSet = [];
+  var matchingBeaconsNewSet = [];
+  mapOfMatchingBeacons.forEach((newBeaconIndex, knownBeaconIndex) => {
+    if (newBeaconIndex != undefined) {
+      matchingBeaconsBaseSet.push(baseBeaconSet[knownBeaconIndex]);
+      matchingBeaconsNewSet.push(newBeacons[newBeaconIndex]);
+    }
+  });
+  return { matchingBeaconsBaseSet, matchingBeaconsNewSet };
+}
+
+function getMapOfMatchingBeacons(beaconsInA, beaconsInB) {
+  const vectorsInCoordinateA_ByBeacon = getVectorsBetweenBeacons(beaconsInA);
+  var vectorsInCoordinateB_byBeacon = getVectorsBetweenBeacons(beaconsInB);
+  var numberOfMatchingMagnitudes_byBeacon = vectorsInCoordinateA_ByBeacon.map(
+    (vectorsInA) => {
+      return vectorsInCoordinateB_byBeacon.map((vectorsInB) => {
+        return countMatchingMagnitudes(vectorsInA, vectorsInB);
+      });
+    }
+  );
+
+  var mapping = numberOfMatchingMagnitudes_byBeacon.map(
+    (matchingMagnitudes) => {
+      const maxCount = Math.max(...matchingMagnitudes);
+      if (maxCount >= 12) {
+        matchingMagnitudes = matchingMagnitudes.indexOf(maxCount);
+      } else {
+        matchingMagnitudes = undefined;
+      }
+      return matchingMagnitudes;
+    }
+  );
+
+  return mapping;
+}
+
+function getOverlappingSensors(data) {
+  var overlappingSensors = [];
+  for (var i = 0; i < data.length; i = i + 1) {
+    for (var j = i; j < data.length; j = j + 1) {
+      if (i == j) {
+        continue;
+      }
+      var match = getMatchedBeacons(data[i].beacons, data[j].beacons);
+      if (match.thereAreMatches) {
+        overlappingSensors.push({ match, scanner1: i, scanner2: j });
+      }
+    }
+  }
+  return overlappingSensors;
+}
+
+function solvePart1(data) {
+  console.log("Solving Part 1");
+  data = parseData(data);
+
+  var beaconsInBaseCoordinateSystem = new Array(data.length);
+  beaconsInBaseCoordinateSystem[0] = data[0]; // Use Scanner0 as the reference system
+  var mappingFunctions = new Array(data.length);
+  mappingFunctions[0] = (vector) => vector; // No mapping required.
+
+  var overlappingSensors = getOverlappingSensors(data);
+
+  var overlap = overlappingSensors.shift();
+
+  do {
+    // console.log(overlap);
+    var mappingFunctionScanner1 = mappingFunctions[overlap.scanner1];
+    var mappingFunctionScanner2 = mappingFunctions[overlap.scanner2];
+    if (!mappingFunctionScanner1 && !mappingFunctionScanner2) {
+      overlappingSensors.push(overlap);
+    } else {
+      var knownBeacons = !mappingFunctionScanner1
+        ? overlap.match.matchingBeaconsNewSet
+        : overlap.match.matchingBeaconsBaseSet;
+      var newBeacons = !mappingFunctionScanner1
+        ? overlap.match.matchingBeaconsBaseSet
+        : overlap.match.matchingBeaconsNewSet;
+      var referenceMappingFunction = !mappingFunctionScanner1
+        ? mappingFunctionScanner2
+        : mappingFunctionScanner1;
+      var newScannerIndex = !mappingFunctionScanner1
+        ? overlap.scanner1
+        : overlap.scanner2;
+
+      var referenceBeacons = knownBeacons.map((b) =>
+        referenceMappingFunction(b)
+      );
+      var { mappingFunction, offset, mappedBeacons } = mapMatchedBeaconSet(
+        referenceBeacons,
+        newBeacons
+      );
+      // console.log({ newScannerIndex });
+      // console.log({ offset });
+      mappingFunctions[newScannerIndex] = mappingFunction;
+    }
+    overlap = overlappingSensors.shift();
+  } while (overlap);
+
+  var mappedVectors = data
+    .map((d, index) => {
+      var map = mappingFunctions[index];
+      return d.beacons.map((vector) => map(vector));
+    })
+    .flat();
+  var uniqueBeacons = [
+    ...new Set(mappedVectors.map((vector) => JSON.stringify(vector))),
+  ].map((str) => JSON.parse(str));
+  console.log(uniqueBeacons.length);
+  process.exit();
+
+  var baseBeaconSet = data.shift().beacons;
+
+  var newBeaconSet = data.shift();
+  do {
+    // console.log(baseBeaconSet);
+    var { mappedBeacons } = fooBar(baseBeaconSet, newBeaconSet);
+    if (mappedBeacons.length) {
+      baseBeaconSet = baseBeaconSet.concat(mappedBeacons);
+      baseBeaconSet = [
+        ...new Set(baseBeaconSet.map((b) => JSON.stringify(b))),
+      ].map((b) => JSON.parse(b));
+    } else {
+      data.push(newBeaconSet);
+    }
+    newBeaconSet = data.shift();
+  } while (newBeaconSet);
+
+  console.log(baseBeaconSet.length);
 }
 
 function isSameCoordinateSystem(matchingBeacons0, matchingBeacons1) {
